f4b9105d0f08a50c9fe1822866c460330677c761
andriy-shevchuk <a.shevchuk.ua@gmail.com>
2017-09-13 23:43:07 +0300

    Start implementing Export to Excel functionality

--- src/main/java/actions/MainApp.java
+++ src/main/java/actions/MainApp.java
@@ -9,13 +9,22 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 public class MainApp extends Application {
 
     TableView<Product> table;
+    private static final String FILE_NAME = "C:\\Users\\Demonologist\\Documents\\MyFirstExcel.xlsx";
+
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -46,18 +55,19 @@ public class MainApp extends Application {
         TextField textField = new TextField();
         textField.setPrefWidth(450);
         textField.setPromptText("Ebay store URL");
-        Button button = new Button("Scrape");
+        Button buttonScrape = new Button("Scrape");
+        Button buttonExportXLS = new Button("Export to Excel");
 
 
 
 
-        hBox.getChildren().setAll(textField,button);
+        hBox.getChildren().setAll(textField,buttonScrape,buttonExportXLS);
         vBox.getChildren().setAll(table);
 
         root.setTop(hBox);
         root.setCenter(vBox);
 
-        button.setOnAction(e -> {
+        buttonScrape.setOnAction(e -> {
             EbayStoreAction storeAction = new EbayStoreAction(textField.getText());
             List[] productsData = storeAction.doScrape();
             int elementNumber = 1;
@@ -70,6 +80,30 @@ public class MainApp extends Application {
                 elementNumber++;
             }
             storeAction.closeBrowser();
+        });
+
+        final int[] rowNum = {0};
+        buttonExportXLS.setOnAction(e -> {
+            XSSFWorkbook workbook = new XSSFWorkbook();
+            XSSFSheet sheet = workbook.createSheet("Ebay Store");
+
+            table.getItems().forEach(c -> {
+                Row row = sheet.createRow(rowNum[0]++);
+                Cell cell = row.createCell(0);
+                cell.setCellValue(c.getName());
+
+
+            });
+
+            try {
+                FileOutputStream outputStream = new FileOutputStream(FILE_NAME);
+                workbook.write(outputStream);
+                workbook.close();
+            } catch (FileNotFoundException ex) {
+                ex.printStackTrace();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
         });
 
         Scene scene = new Scene(root);
